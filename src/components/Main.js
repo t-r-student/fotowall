26edea9803ca466625c9eb0a83482755b312f978
t-r-student <kst369@mail.ru>
2019-05-14 10:27:34 +0200

    solution for issue #1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,27 +2,29 @@ import React from 'react';
 import Title from './Title';
 import Photowall from './Photowall';
 
-const posts = [{
-    id: "0",
-    description: "beautifull landscape",
-    imageLink: "http://costaszissis.com/wp-content/uploads/2015/06/nature_05-400x400.jpg"
-},
-{
-    id: "1",
-    description: "sport",
-    imageLink: "http://media.ussportscamps.com/media/images/basketball/nike/_400x400_crop_top-center_75/nike_thumbnail_400x400_1.jpg"
-},
-
-
-]
 
 class Main extends React.Component{
+    constructor(){
+        super()
+        this.state= {
+            posts : [{
+                id: "0",
+                description: "beautifull landscape",
+                imageLink: "http://costaszissis.com/wp-content/uploads/2015/06/nature_05-400x400.jpg"
+            },
+            {
+                id: "1",
+                description: "sport",
+                imageLink: "http://media.ussportscamps.com/media/images/basketball/nike/_400x400_crop_top-center_75/nike_thumbnail_400x400_1.jpg"
+            }]
+        }
+    }
     render(){
         return <div>
             <Title title = {'Fotowall'}/>
                 <div className="photoGrid">
                     <div className="row">
-                        <Photowall posts={posts}/>
+                        <Photowall posts={this.state.posts}/>
                     </div>
                 </div>
             </div>
