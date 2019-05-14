eac8d0f7b70ecf7110e926e4f03a57f8d2133c30
t-r-student <kst369@mail.ru>
2019-05-14 15:09:30 +0200

    solution for issue #5

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,19 @@ class Main extends React.Component{
     constructor(){
         super()
         this.state= {
-            posts : []
+            posts : [{
+                id: "0",
+                description: "beautifull landscape",
+                imageLink: "http://costaszissis.com/wp-content/uploads/2015/06/nature_05-400x400.jpg"
+            },
+            {
+                id: "1",
+                description: "sport",
+                imageLink: "http://media.ussportscamps.com/media/images/basketball/nike/_400x400_crop_top-center_75/nike_thumbnail_400x400_1.jpg"
+            }]
         }
         this.removePhoto = this.removePhoto.bind(this);
+        console.log('constructor');
     }
 
 
@@ -21,14 +31,13 @@ class Main extends React.Component{
         }))
     }
 
-    componentDidMount(){
-        const data = SimulateFetchFromDB();
-        this.setState({
-            posts : data
-        })
+    componentDidUpdate(prevProps, prevState){
+        console.log(prevState.posts);
+        console.log(this.state);
     }
 
     render(){
+        console.log('render');
         return <div>
             <Title title = {'Fotowall'}/>
                 <div className="photoGrid">
@@ -41,16 +50,7 @@ class Main extends React.Component{
 }
 
 function SimulateFetchFromDB(){
-    return [{
-        id: "0",
-        description: "beautifull landscape",
-        imageLink: "http://costaszissis.com/wp-content/uploads/2015/06/nature_05-400x400.jpg"
-    },
-    {
-        id: "1",
-        description: "sport",
-        imageLink: "http://media.ussportscamps.com/media/images/basketball/nike/_400x400_crop_top-center_75/nike_thumbnail_400x400_1.jpg"
-    }]
+    return []
 }
 
 export default Main;
